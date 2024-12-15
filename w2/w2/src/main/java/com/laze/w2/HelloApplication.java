b939ff060755f968e9e32ea15bc3710272797451
L-a-z-e <yysi8771@gmail.com>
2024-12-15 15:13:39 +0900

    w2 init

---
+++ w2/w2/src/main/java/com/laze/w2/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.laze.w2;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
