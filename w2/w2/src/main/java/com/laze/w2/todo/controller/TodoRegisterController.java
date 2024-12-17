be19534a2e1e64b9a6c22b6530897a22e5025441
L-a-z-e <yysi8771@gmail.com>
2024-12-17 15:07:00 +0900

    chapter3

--- w2/w2/src/main/java/com/laze/w2/todo/controller/TodoRegisterController.java
+++ w2/w2/src/main/java/com/laze/w2/todo/controller/TodoRegisterController.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -23,7 +24,20 @@ public class TodoRegisterController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        System.out.println("TodoRegisterController > doGet");
+        log.info("/todo/register doGet");
+
+        HttpSession session = req.getSession();
+
+        if (session.isNew()) {
+            log.info("new JSESSIONID");
+            resp.sendRedirect("/login");
+        }
+
+        if (session.getAttribute("loginInfo") == null) {
+           log.info("loginInfo is null");
+           resp.sendRedirect("/login");
+           return;
+        }
 
         req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req, resp);
 
