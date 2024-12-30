0b3fde3c90880d4d69d19dc3609f3513363da1c7
L-a-z-e <yysi8771@gmail.com>
2024-12-30 23:02:21 +0900

    .

--- bootex/src/main/java/com/laze/bootex/controller/SampleJSONController.java
+++ bootex/src/main/java/com/laze/bootex/controller/SampleJSONController.java
@@ -11,7 +11,6 @@ public class SampleJSONController {
     @GetMapping("/helloArr")
     public String[] helloArr() {
         log.info("helloArr");
-        log.info("helloArr2");
         return new String[]{"AAA", "BBB", "CCC"};
     }
 }
