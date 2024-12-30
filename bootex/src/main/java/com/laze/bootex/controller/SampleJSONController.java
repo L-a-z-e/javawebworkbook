c494a956c1c1794c552ff99e4a724faeffcad92a
L-a-z-e <yysi8771@gmail.com>
2024-12-30 23:02:02 +0900

    .

---
+++ bootex/src/main/java/com/laze/bootex/controller/SampleJSONController.java
@@ -0,0 +1,17 @@
+package com.laze.bootex.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+public class SampleJSONController {
+
+    @GetMapping("/helloArr")
+    public String[] helloArr() {
+        log.info("helloArr");
+        log.info("helloArr2");
+        return new String[]{"AAA", "BBB", "CCC"};
+    }
+}
---
+++ bootex/src/test/java/com/laze/bootex/BootexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laze.bootex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
